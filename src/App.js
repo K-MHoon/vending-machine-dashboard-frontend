8f81b49b023dd896faf4d79f50871c652a258baf
K-MHOON <azmria117@gmail.com>
2024-10-27 23:33:24 +0900

    feat : Router, ReactQuery 설정, cookieUtil 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import root from "./router/root";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={root} />
+            <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
