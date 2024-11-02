ca3161ff820b116d8f94a02e0303908b6028f0c0
K-MHOON <azmria117@gmail.com>
2024-11-02 21:00:52 +0900

    feat : 로그인 정보 관련 hook 추가

---
+++ src/hooks/useCustomLogin.js
@@ -0,0 +1,81 @@
+import { createSearchParams, Navigate, useNavigate } from "react-router-dom";
+import { useRecoilState, useResetRecoilState } from "recoil";
+import siginInState from "../atoms/signinState";
+import { removeCookie, setCookie } from "../util/cookieUtil";
+import { loginPost } from "../api/memberApi";
+
+const useCustomLogin = () => {
+    const navigate = useNavigate();
+
+    const [loginState, setLoginState] = useRecoilState();
+
+    const resetState = useResetRecoilState(siginInState);
+
+    const isLogin = loginState.email ? true : false;
+
+    const doLogin = async (loginParam) => {
+        const result = await loginPost(loginParam);
+
+        console.log(result);
+
+        saveAsCookie(result);
+
+        return result;
+    };
+
+    const saveAsCookie = (data) => {
+        setCookie("member", JSON.stringify(data), 1);
+        setLoginState(data);
+    };
+
+    const doLogout = () => {
+        removeCookie("member");
+        resetState();
+    };
+
+    const moveToPath = (path, replace) => {
+        navigate({ pathname: path }, { replace: replace });
+    };
+
+    const moveToLogin = () => {
+        navigate({ pathname: "/member/login" }, { replace: true });
+    };
+
+    const moveToLoginReturn = () => {
+        return <Navigate replace to="/member/login" />;
+    };
+
+    const exceptionHandler = (ex) => {
+        console.log("Exception");
+        console.log(ex);
+
+        const errorMsg = ex.response.data.error;
+        const errorStr = createSearchParams({ error: errorMsg }).toString();
+
+        if (errorMsg === "REQUIRE_LOGIN") {
+            alert("로그인 해야 합니다.");
+            navigate({ pathname: "/member/login", search: errorStr });
+            return;
+        }
+
+        if (errorMsg === "ERROR_ACCESS_DENIED") {
+            alert("해당 메뉴를 사용할 수 있는 권한이 없습니다.");
+            navigate({ pathname: "/member/login", search: errorStr });
+            return;
+        }
+    };
+
+    return {
+        loginState,
+        isLogin,
+        doLogin,
+        doLogout,
+        moveToPath,
+        moveToLogin,
+        moveToLoginReturn,
+        exceptionHandler,
+        saveAsCookie,
+    };
+};
+
+export default useCustomLogin;
