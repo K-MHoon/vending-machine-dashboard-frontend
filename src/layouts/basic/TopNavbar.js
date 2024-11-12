e15c66fe247a3371096d6a54031cb71220d7b766
K-MHOON <azmria117@gmail.com>
2024-11-12 22:55:07 +0900

    fix : 레이아웃 이동 및 MainPage > DashboardsPage로 변경

--- src/layouts/basic/TopNavbar.js
+++ src/layouts/basic/TopNavbar.js
@@ -15,9 +15,13 @@ const TopNavbar = () => {
                     K-MHOON
                 </Link>
                 <nav className="hidden space-x-3 md:flex md:items-center">
-                    <label class="inline-flex items-center cursor-pointer">
-                        <input type="checkbox" value="" class="sr-only peer" />
-                        <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-emerald-400 after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-emerald-400 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-slate-400"></div>
+                    <label className="inline-flex items-center cursor-pointer">
+                        <input
+                            type="checkbox"
+                            value=""
+                            className="sr-only peer"
+                        />
+                        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-emerald-400 after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-emerald-400 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-slate-400"></div>
                     </label>
                     <button className="p-3 bg-emerald-800 rounded-full w-[50px] h-[50px] text-white text-center">
                         <FaBell className="w-full h-full" />
--- src/router/root.js
+++ src/router/root.js
@@ -2,14 +2,14 @@ import React, { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 const Loading = <div>Loading....</div>;
-const Main = lazy(() => import("../pages/MainPage"));
+const Dashboards = lazy(() => import("../pages/dashboards/DashboardsPage"));
 
 const root = createBrowserRouter([
     {
         path: "",
         element: (
             <Suspense fallback={Loading}>
-                <Main />
+                <Dashboards />
             </Suspense>
         ),
     },
